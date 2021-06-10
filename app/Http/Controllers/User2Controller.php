c2c5b524c93dc2297b1300c606a856a63afdcaef
unknown <roselleannguzman14@gmail.com>
2021-06-10 18:12:23 +0800

    latest changes

--- app/Http/Controllers/User2Controller.php
+++ app/Http/Controllers/User2Controller.php
@@ -1,73 +1,78 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-//use App\User;
-// use App\Models\User;  // <-- your model is located insired Models Folder
-use Illuminate\Http\Response; // Response Components
-use App\Traits\ApiResponser;  // <-- use to standardized our code for api response
-use Illuminate\Http\Request;  // <-- handling http request in lumen
-use DB; // <-- if your not using lumen eloquent you can use DB component in lumen
-use App\Services\User2Service; // user2 Services
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Response;
+    //use App\Models\User;
+    use App\Traits\ApiResponser;
+    use App\Services\User2Service;
+    use DB;
 
+    Class User2Controller extends Controller {
+        use ApiResponser;
 
-class User2Controller extends Controller {
-    // use to add your Traits ApiResponser
-    use ApiResponser;
-
-    /**
-     * The service to consume the User2 Microservice
-     * @var User2Service
-     */
-    public $user2Service;
-
-    /**
-     * Create a new controller instance
-     * @return void
-     */
-    public function __construct(User2Service $user2Service){
+        /**
+         * The service to consume the User1 Microservice
+         * @var User2Service
+         */
+        public $user2Service;
+        /**
+        * Create a new controller instance
+        * @return void
+        */
+        public function __construct(User2Service $user2Service){
         $this->user2Service = $user2Service;
+        }
+        /*public function getUsers(){
+           
+        }
+         
+        /**
+        * Return the list of users
+        * @return Illuminate\Http\Response
+        */
+        public function index()
+        {
+            //
+            return $this->successResponse($this->user2Service->obtainUsers2());
+        }
+
+
+        public function addUser(Request $request)
+        {
+            return $this->successResponse($this->user2Service->createUser2($request->all(),Response::HTTP_CREATED));
+        }
+
+        /**
+        * Obtains and show one user
+        * @return Illuminate\Http\Response
+        */
+
+        public function show($id)
+        {
+            return $this->successResponse($this->user2Service->obtainUser2($id));
+        }
+
+        /**
+         * Update an existing user
+         * @return Illuminate\Http\Response
+        */
+
+        public function update(Request $request,$id)
+        {
+            return $this->successResponse($this->user2Service->editUser2($request->all(),$id));
+        }
+
+        /**
+        * Remove an existing user
+        * @return Illuminate\Http\Response
+        */
+
+        public function delete($id)
+        {
+            return $this->successResponse($this->user2Service->deleteUser2($id));
+        }
     }
 
-    /**
-     * Return the list of users
-     * @return Illuminate\Http\Response
-     */
-    public function index()
-    {
-        return $this->successResponse($this->user2Service->obtainUsers());
-    }
-
-    public function add(Request $request ){
-        return $this->successResponse($this->user2Service->createUser($request->all(), Response::HTTP_CREATED));
-    }
-
-    /**
-     * Obtains and show one user
-     * @return Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        return $this->successResponse($this->user2Service->obtainUser($id));
-    }
-
-    /**
-     * Update an existing user
-     * @return Illuminate\Http\Response
-     */
-    public function update(Request $request,$id)
-    {
-        return $this->successResponse($this->user2Service->editUser($request->all(), $id));
-    }
-
-    /**
-     * Remove an existing user
-     * @return Illuminate\Http\Response
-     */
-    public function delete($id)
-    {
-        return $this->successResponse($this->user2Service->deleteUser($id));
-    }
-
-}
 ?>
--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,30 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['middleware' => 'client.credentials'], function () use ($router){
+//no authentication
+//$router->group([], function() use ($router) {
 
-    $router->get('/users1', 'User1Controller@index'); // get all users records
-    $router->post('/users1', 'User1Controller@add'); // create new user record
-    $router->get('/users1/{id}', 'User1Controller@show'); // get user by id
-    $router->put('/users1/{id}', 'User1Controller@update'); // update user record
-    $router->patch('/users1/{id}', 'User1Controller@update'); // update user record
-    $router->delete('/users1/{id}', 'User1Controller@delete'); // delete record
+//with authentication
+$router->group(['middleware' => 'client.credentials'], function () use ($router) {
 
-    $router->get('/users2', 'User2Controller@index'); // get all users records
-    $router->post('/users2', 'User2Controller@add'); // create new user record
-    $router->get('/users2/{id}', 'User2Controller@show'); // get user by id
-    $router->put('/users2/{id}', 'User2Controller@update'); // update user record
-    $router->patch('/users2/{id}', 'User2Controller@update'); // update user record
-    $router->delete('/users2/{id}', 'User2Controller@delete'); // delete record
+//API gateway routes for SITE1 users
+$router->get('/users1', 'User1Controller@index');
+$router->post('/users1', 'User1Controller@addUser');
+$router->get('/users1/{id}', 'User1Controller@show');
+$router->put('/users1/{id}', 'User1Controller@update');
+$router->patch('/users1/{id}', 'User1Controller@update');
+$router->delete('/users1/{id}', 'User1Controller@delete');
+
+//API gateway routes for SITE2 users
+$router->get('/users2', 'User2Controller@index');
+$router->post('/users2', 'User2Controller@addUser');
+$router->get('/users2/{id}', 'User2Controller@show');
+$router->put('/users2/{id}', 'User2Controller@update');
+$router->patch('/users2/{id}', 'User2Controller@update');
+$router->delete('/users2/{id}', 'User2Controller@delete');
+
+});
+
+$router->group(['middleware' => 'auth:api'], function () use ($router) {
+    $router->get('/users/me', 'UserController@me');
 });
