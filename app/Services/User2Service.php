888fd97acf17bfac8c406488861d7e2ab6b81125
unknown <roselleannguzman14@gmail.com>
2021-06-11 09:29:03 +0800

    Latest changes

--- app/Services/User2Service.php
+++ app/Services/User2Service.php
@@ -1,79 +1,77 @@
 <?php
+    namespace App\Services;
+    use App\Traits\ConsumesExternalService;
 
-namespace App\Services;
+    class User2Service{
+        
+        use ConsumesExternalService;
+        /**
+        * The base uri to consume the User2 Service
+        * @var string
+        */
+        
+        public $baseUri;
 
-use App\Traits\ConsumesExternalService;
+        /**
+         * The secret to consume the User1 Service
+         * @var string
+         */
+        
+        public $secret;
+        
+        public function __construct()
+        {
+            $this->baseUri = config('services.users2.base_uri');
+            $this->secret = config('services.users2.secret');
+        }
 
-class User2Service{
+        /**
+        * Obtain the full list of Users from User1 Site
+        * @return string
+        */
 
-    use ConsumesExternalService;
+        public function obtainUsers()
+        {
+            return $this->performRequest('GET', '/users'); 
+        }
 
-    /**
-     * The base uri to consume the User2 Service
-     * @var string
-     */
-    public $baseUri;
+        /**
+        * Create one user using the User2 service
+        * @return string
+        */
 
-    /**
-     * The secret to consume the User2 Service
-     * @var string
-     */
-    public $secret;
+        public function createUser($data)
+        {
+            return $this->performRequest('POST', '/users', $data);
+        }
 
-    public function __construct()
-    {
-        $this->baseUri = config('services.users2.base_uri');
-        $this->secret = config('services.users2.secret');
-    }
+        /**
+        * Obtain the full list of Users from User2 Site
+        * @return string
+        */
 
-    /**
-     * Obtain the full list of Users from User2 Site
-     * @return string
-     */
-    public function obtainUsers2()
-    {
-        return $this->performRequest('GET','/users');
-    }
-
-    /**
-     * Create one user using the User2 service
-     * @return string
-     */
-    public function createUser2($data)
-    {
-        return $this->performRequest('POST', '/users', $data);
-    }
-
-    public function obtainUserJob($id)
-    {
-        return $this->performRequest('GET', "/userjob/{$id}");
-    }
-
-    /**
-     * Obtain one single user from the User2 service
-     * @return string
-     */
-    public function obtainUser2($id)
-    {
-        return $this->performRequest('GET', "/users/{$id}");
-    }
-
-    /**
-     * Update an instance of user2 using the User2 service
-     * @return string
-     */
-    public function editUser2($data, $id)
-    {
+        public function obtainUser($id)
+        {
+            return $this->performRequest('GET', "/users/{$id}");
+        }
+        
+        /**
+        * Update an instance of user2 using the User2 service
+        * @return string
+        */
+        
+        public function editUser($data, $id)
+        {
         return $this->performRequest('PUT', "/users/{$id}", $data);
-    }
+        }
 
-    /**
-     * Remove a single user using the User2 service
-     * @return string
-     */
-    public function deleteUser2($id)
-    {
-        return $this->performRequest('DELETE', "/users/{$id}");
-    }
+        /**
+        * Remove an existing user
+        * @return Illuminate\Http\Response
+        */
 
-}
+        public function deleteUser($id)
+        {
+            return $this->performRequest('DELETE', "/users/{$id}");
+        }
+    }
